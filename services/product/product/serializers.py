9190691be3378da9e0c902ea55c93279f0609e18
hannes f <hannes.furhoff@hotmail.com>
2023-12-07 14:51:36 +0100

    Cleanup

--- services/product/product/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id', 'category', 'price', 'stock', 'name', 'description']
--- webserver/eshop/views.py
+++ webserver/eshop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 # User views
 def home(request):

0390706ea3a624be2749fcf1064ccd03935e14df
WarsayMaharena <warmah-0@student.ltu.se>
2023-12-29 14:08:32 +0100

    added lookup functions in views.py

--- services/ProductService/ProductApp/views.py
+++ services/ProductService/ProductApp/views.py
@@ -9,10 +9,45 @@ from .queries import *
 
 
 lookup = {
-    "get_event":{
+    "create_event":{
         "func": event_Insert_Query,
         "groups":["admin"],
-        "required_fields":[("id", int),("category", str)]
+        "required_fields":[("event_name", str)]
+    },
+        "create_product":{
+        "func": create_Product,
+        "groups":["admin"],
+        "required_fields":[("category_id", int), ("product_price", int), ("product_stock",int), ("product_name", str), ("product_description",str)]
+    },
+        "create_product_discount":{
+        "func": product_Discount_Insert_Query,
+        "groups":["admin"],
+        "required_fields":[("discount", int), ("special_sale_id",int), ("product_id",int)]
+    },
+        "create_category":{
+        "func": category_Insert_Query,
+        "groups":["admin"],
+        "required_fields":[("name", str)]
+    },
+        "delete_event":{
+        "func": event_Delete_Query,
+        "groups":["admin"],
+        "required_fields":[("event_name", str)]
+    },
+        "delete_product":{
+        "func": product_Delete_Query,
+        "groups":["admin"],
+        "required_fields":[("product_id", int)]
+    },
+        "delete_product_discount":{
+        "func": product_Discount_Delete_Query,
+        "groups":["admin"],
+        "required_fields":[("product_id", int)]
+    },
+        "delete_category":{
+        "func": category_Delete_Query,
+        "groups":["admin"],
+        "required_fields":[("category_id", str)]
     }
 }
 

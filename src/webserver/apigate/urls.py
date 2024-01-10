7fa70c00a18a0a412faa6661de7e1aa49c3c9472
hannes f <hannes.furhoff@hotmail.com>
2024-01-10 19:33:02 +0100

    fixes #142415261256

--- src/webserver/apigate/urls.py
+++ src/webserver/apigate/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # CRUD for all DB tables
-    path('product/<int:id>/', views.handle_product_crud, name="crud-product"),
+    path('product/', views.handle_product_crud, name="crud-product"),
     path('category/<str:name>/', views.handle_category_crud, name="crud-category"),
     path('user', views.handle_user_crud, name='crud-user'),
     path('user/<int:id>/', views.handle_user_crud, name='crud-user'),
--- src/webserver/apigate/views.py
+++ src/webserver/apigate/views.py
@@ -22,20 +22,16 @@ def validateCall(req):
 
 # Handle Product table CRUD
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def handle_product_crud(request, id):
+def handle_product_crud(request):
     match(request.method):
         case    "GET": response = api_gateway_client.call("prod", "get-product",
-                                                           {"id": id}, 
-                                                           None, None)
-        case   "POST": response = api_gateway_client.call("prod", "update-product", 
-                                                           {"id": id, "post-data": request.data}, 
-                                                           None, None)
+                                                           request.data, validateCall(request))
+        case   "POST": response = api_gateway_client.call("prod", "get-product", 
+                                                           request.data, validateCall(request))
         case    "PUT": response = api_gateway_client.call("prod", "create-product",
-                                                           {"id":id, "put-data": request.data}, 
-                                                           None, None)
+                                                           request.data, validateCall(request))
         case "DELETE": response = api_gateway_client.call("prod", "delete-product", 
-                                                           {"product_id":id}, 
-                                                           None, None)
+                                                           request.data, validateCall(request))
         case _       : response = protocol.undefined_logic_response
     return JsonResponse(response)
 
